75d371ba785e39956542b586b007d2cb3b1ef5f0
Mark Williams <mwill@Marks-MacBook-Pro.local>
2024-09-18 13:57:08 -0400

    failing tests with initial interface design

---
+++ bottles/bottles.py
@@ -0,0 +1,15 @@
+class Bottles:
+    def __init__(self):
+        pass
+
+    def verses(self, start: int, stop: int) -> str:
+        """Lyrics for all verses from `start` bottles to `stop` bottles."""
+        return ""
+
+    def verse(self, bottles: int) -> str:
+        """The verse for a particular number of `bottles` on the wall."""
+        return ""
+
+    def song(self) -> str:
+        """Lyrics all verses from 99 to 0 bottles on the wall."""
+        return ""
